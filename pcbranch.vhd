896020ba3ed361c08683957911daf6f7c2c8e167
Benjamin Greenwood <bgreenwood22@my.whitworth.edu>
2021-04-17 15:13:53 -0700

    added missing semicolon

--- pcbranch.vhd
+++ pcbranch.vhd
@@ -10,7 +10,7 @@ entity pcbranch is     -- define signals going in and out of the adder
   port(
        constant_start: in STD_LOGIC_VECTOR(31 downto 0);
        offset: in STD_LOGIC_VECTOR(18 downto 0);
-       Result: out STD_LOGIC_VECTOR(31 downto 0)
+       Result: out STD_LOGIC_VECTOR(31 downto 0);
        );
 end;
 
